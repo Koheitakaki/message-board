26ea4ee1d18af208cfe99a335f7911dd903eb789
kohei.LD <NA>
2018-04-02 10:24:32 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328045225) do
+ActiveRecord::Schema.define(version: 20180402101355) do
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
   end
 
 end
